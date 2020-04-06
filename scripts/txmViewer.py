d54b9d3ad7dc4ad0d605e22c07bc999f43ccd022
jhektor <johan.hektor@gmail.com>
2020-04-06 14:43:44 +0200

    updated layout and histograms

--- scripts/txmViewer.py
+++ scripts/txmViewer.py
@@ -4,6 +4,7 @@ import textwrap
 
 import dxchange.reader as dxreader
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 from matplotlib.widgets import Slider
 
 
@@ -13,29 +14,36 @@ def read_txm(file, slices=None):
 
 
 def slice_viewer(data, metadata=None, **kwargs):
-    fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3)
-    plt.subplots_adjust(left=0.25, bottom=0.25)
+    fig = plt.figure(figsize=(10, 8))
+    gs = GridSpec(5, 3, figure=fig, height_ratios=[8, 3, 0.2, 0.2, 0.2])
+    # image axes
+    ax_xy = fig.add_subplot(gs[0, 0])
+    ax_xz = fig.add_subplot(gs[0, 1])
+    ax_yz = fig.add_subplot(gs[0, 2])
+    plt.subplots_adjust(hspace=0.25)
     # plot images
-    imxy = ax1.imshow(data[data.shape[0] // 2])
-    imxz = ax2.imshow(data[:, data.shape[1] // 2, :])
-    imyz = ax3.imshow(data[:, :, data.shape[2] // 2])
-    ax1.set_title('Top view')
-    ax2.set_title('Side view')
-    ax3.set_title('Front view')
+    imxy = ax_xy.imshow(data[data.shape[0] // 2], **kwargs)
+    imxz = ax_xz.imshow(data[:, data.shape[1] // 2, :], **kwargs)
+    imyz = ax_yz.imshow(data[:, :, data.shape[2] // 2], **kwargs)
+    ax_xy.set_title('Top view')
+    ax_xz.set_title('Side view')
+    ax_yz.set_title('Front view')
 
-    for ax, color in zip([ax1, ax2, ax3], ['black', 'blue', 'red']):
-        plt.setp(ax.spines.values(), color=color)
-    # plot histograms
-    ax4.hist(data[data.shape[0] // 2].flatten())
-    ax5.hist(data[:, data.shape[1] // 2, :].flatten())
-    ax6.hist(data[:, :, data.shape[2] // 2].flatten())
+    for ax, color in zip([ax_xy, ax_xz, ax_yz], ['green', 'blue', 'red']):
+        plt.setp(ax.spines.values(), color=color, linewidth=2)
 
-    # set up slider
-    # ax.margins(x=0)
+    # histogram axis
+    ax_h = fig.add_subplot(gs[1, :], )
+    ax_h.hist((data[data.shape[0] // 2].flatten(), data[:, data.shape[1] // 2, :].flatten(),
+               data[:, :, data.shape[2] // 2].flatten()), color=('g', 'b', 'r'), label=('Top', 'Side', 'Front'),
+              bins=100)
+    ax_h.legend()
+
+    # set up sliders
     axcolor = 'lightgoldenrodyellow'
-    axslice_xy = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
-    axslice_xz = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
-    axslice_yz = plt.axes([0.25, 0.05, 0.65, 0.03], facecolor=axcolor)
+    axslice_xy = fig.add_subplot(gs[2, :], facecolor=axcolor)
+    axslice_xz = fig.add_subplot(gs[3, :], facecolor=axcolor)
+    axslice_yz = fig.add_subplot(gs[4, :], facecolor=axcolor)
     sslice_xy = Slider(axslice_xy, 'Slice top', 0, data.shape[0] - 1, valinit=data.shape[0] // 2, valstep=1)
     sslice_xz = Slider(axslice_xz, 'Slice side', 0, data.shape[1] - 1, valinit=data.shape[1] // 2, valstep=1)
     sslice_yz = Slider(axslice_yz, 'Slice front', 0, data.shape[2] - 1, valinit=data.shape[2] // 2, valstep=1)
@@ -43,20 +51,20 @@ def slice_viewer(data, metadata=None, **kwargs):
     # draw lines indicating slices
     def clear_lines():
         for i in range(2):
-            ax1.mylines[i].remove()
-            ax2.mylines[i].remove()
-            ax3.mylines[i].remove()
+            ax_xy.mylines[i].remove()
+            ax_xz.mylines[i].remove()
+            ax_yz.mylines[i].remove()
 
     def draw_lines(sxy, sxz, syz):
-        ax1.mylines = []
-        ax2.mylines = []
-        ax3.mylines = []
-        ax1.mylines.append(ax1.axhline(int(sxz), alpha=0.5, color='blue'))
-        ax1.mylines.append(ax1.axvline(int(syz), alpha=0.5, color='red'))
-        ax2.mylines.append(ax2.axhline(int(sxy), alpha=0.5, color='black'))
-        ax2.mylines.append(ax2.axvline(int(syz), alpha=0.5, color='red'))
-        ax3.mylines.append(ax3.axhline(int(sxy), alpha=0.5, color='black'))
-        ax3.mylines.append(ax3.axvline(int(sxz), alpha=0.5, color='blue'))
+        ax_xy.mylines = []
+        ax_xz.mylines = []
+        ax_yz.mylines = []
+        ax_xy.mylines.append(ax_xy.axhline(int(sxz), alpha=0.5, color='blue'))
+        ax_xy.mylines.append(ax_xy.axvline(int(syz), alpha=0.5, color='red'))
+        ax_xz.mylines.append(ax_xz.axhline(int(sxy), alpha=0.5, color='green'))
+        ax_xz.mylines.append(ax_xz.axvline(int(syz), alpha=0.5, color='red'))
+        ax_yz.mylines.append(ax_yz.axhline(int(sxy), alpha=0.5, color='green'))
+        ax_yz.mylines.append(ax_yz.axvline(int(sxz), alpha=0.5, color='blue'))
 
     draw_lines(int(sslice_xy.val), int(sslice_xz.val), int(sslice_yz.val))
 
@@ -68,16 +76,15 @@ def slice_viewer(data, metadata=None, **kwargs):
         imxz.set_data(data[:, sxz, :])
         imyz.set_data(data[:, :, syz])
 
-        # Updata histograms
-        ax4.clear()
-        ax5.clear()
-        ax6.clear()
-        ax4.hist(data[sxy].flatten())
-        ax5.hist(data[:, sxz, :].flatten())
-        ax6.hist(data[:, :, syz].flatten())
-
         clear_lines()
         draw_lines(sxy, sxz, syz)
+
+        # Update histograms
+        ax_h.clear()
+        ax_h.hist((data[sxy], data[:, sxz, :],
+                   data[:, :, syz]), color=('g', 'b', 'r'), label=('Top', 'Side', 'Front'), bins=100)
+        ax_h.legend()
+
         fig.canvas.draw_idle()
 
     sslice_xy.on_changed(update)
@@ -102,5 +109,6 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
 
+    print('running')
     data, meta = read_txm(args.file)  # ,slices=((0,10,1),(0,1013,1),(0,988,1)))
-    slice_viewer(data)
+    slice_viewer(data, cmap='bone')
